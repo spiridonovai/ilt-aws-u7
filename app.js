9b63df5ee22cb6d4678ef3a9b81407f7bc79ccbf
spiridonovai <i.spiridonova@yahoo.com>
2018-02-12 13:40:08 +0100

    first

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express')
+
+var app = express()
+
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'me',
+//   password: 'secret',
+//   database: 'my_db'
+// });
+
+// connection.connect();
+
+
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.get('/:name', function (req, res) {
+  res.send('Hello ' + req.params.name)
+})
+// app.get('/test_mysql_connection', function(req,res){
+//   connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error){
+//       res.statusCode = 500;
+//       res.send(error);
+//     }
+//     res.send(results);
+//   });
+// })
+ 
+app.listen(3000)
