220c75b7bef60be89778b1ea802edc8855729415
spiridonovai <i.spiridonova@yahoo.com>
2018-02-13 14:06:34 +0100

    izmeni

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var mysql = require('mysql')
 
 var app = express()
 
+var path = require('path')
+app.use('/Scripts', express.static(path.join(__dirname + '/Scripts')));
+
 var connection = mysql.createConnection({
   host: 'db.c76o0nb7ihuq.eu-central-1.rds.amazonaws.com',
   user: 'iSpiridonova',
@@ -13,6 +16,10 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
+
+app.get('/login', function(req, res){
+  res.sendFile(__dirname + '/Views/login.html');
+})
  
 app.get('/', function (req, res) {
   res.send('Hello lgflf')
@@ -21,7 +28,9 @@ app.get('/', function (req, res) {
 app.get('/name/:name', function (req, res) {
   res.send('Hello ' + req.params.name)
 })
-
+app.get('/charts', function (req, res) {
+  res.sendFile(__dirname + '/Views/charts.html');
+})
 app.get('/test', function(req,res){
   connection.query('SELECT * FROM Grade', function (error, results, fields) {
     if (error){
